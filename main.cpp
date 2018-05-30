ae9d2f00caff6f3be3a3e731b9740c3377c6086e
1uci3n <giranten@outlook.com>
2018-05-30 15:08:05 +0900

    fix comment

--- main.cpp
+++ main.cpp
@@ -2,25 +2,33 @@
 * @Author: 1uci3n
 * @Date:   2018-05-28 16:36:26
 * @Last Modified by:   1uci3n
-* @Last Modified time: 2018-05-29 13:04:32
+* @Last Modified time: 2018-05-30 14:43:27
 */
 #include "NeuralNetwork.h"
 
+//データグループでトレーニング用のデータの数
 #define TRAINING_DATA_COUNT 100
+//ゼロ画像の目標結果
 #define T_FOR_ZERO -1
+//イチ画像の目標結果
 #define T_FOR_ONE 1
-#define ERROR_BOUND 1
+//エラーの結果の限界
+#define ERROR_BOUND 0.1
 
 using namespace std;
 
 //initial weight value
-vector<double> w(256, 0);
+vector<double> w(256);
 
+//main function
 int main(int argc, char const *argv[])
 {
+	srand(time(NULL));
+	initW();
 	//get the data(The first 100 are samples, the remaining 60 are tests)
 	vector<vector <vector<int> > > data0 = getFormatDataGroupFromFile("sample/data0.dat");
 	vector<vector <vector<int> > > data1 = getFormatDataGroupFromFile("sample/data1.dat");
+	//formatdata to datagorup< data< data expanded by row > >
 	vector<vector<int> > formatData0 = formatDataToOneRow(data0);
 	vector<vector<int> > formatData1 = formatDataToOneRow(data1);
 	cout << "----訓練スタート----" << endl;
@@ -36,13 +44,26 @@ int main(int argc, char const *argv[])
 	return 0;
 }
 
+void initW(){
+	for (int i = 0; i < 256; ++i)
+	{
+		w[i] = (rand() % 10) * 0.01;
+	}
+}
+
 void training(vector <vector<int> > datas, int tn){
+	//use this to temporarily store data 
 	vector<int> data;
+	//use this to  store error's index
 	vector<int> errorIndex;
+	//use this to count error
 	int countError = 0;
+	//use this to store the sum of error value
 	double errorSum = 0;
-	vector<int>::iterator it = errorIndex.end();
+	//use this to temporarily store result in the loop
 	double result = 0;
+	vector<int>::iterator it = errorIndex.end();
+	//training loop start
 	while(true){
 		for (int i = 0; i < TRAINING_DATA_COUNT; ++i)
 		{
@@ -93,7 +114,6 @@ void test(vector <vector<int> > datas, int tn){
 		if((result * tn) < 0){
 			sumErr ++;
 		}
-		//cout << result << endl;
 		result = 0;
 	}
 	cout << "識別の誤り率は" << sumErr / 60 << endl;
